5b0650cc310c246e2f1002cbd44be0bfb35ca797
janekeyes <janekeyes123@gmail.com>
2025-04-07 14:44:16 +0100

    initial commit

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,51 @@
+class StudentsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :check_admin, only: [:new, :create, :edit, :update, :destroy]
+  
+    def index
+      @students = Student.all
+    end
+  
+    def new
+      @student = Student.new
+    end
+  
+    def create
+      @student = Student.new(student_params)
+      if @student.save
+        redirect_to students_path, notice: 'Student created successfully.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+      @student = Student.find(params[:id])
+    end
+  
+    def update
+      @student = Student.find(params[:id])
+      if @student.update(student_params)
+        redirect_to students_path, notice: 'Student updated successfully.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @student = Student.find(params[:id])
+      @student.destroy
+      redirect_to students_path, notice: 'Student deleted successfully.'
+    end
+  
+    private
+  
+    def student_params
+      params.require(:student).permit(:name, :email)
+    end
+  
+    def check_admin
+      redirect_to students_path, alert: "You are not authorized to perform this action." unless current_user.role == "admin"
+    end
+  end
+  
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  after_initialize :set_default_role, if: :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+end
