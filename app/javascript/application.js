485bb85d45889141e8876b6bdd05321f7ceaaf7d
janekeyes <janekeyes123@gmail.com>
2025-04-10 20:33:52 +0100

    presence vaildation for student register

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"  
-import "@rails/ujs" 
-Rails.start()  
-import "controllers"  
+import "@hotwired/turbo-rails"  // Uncomment this if using Turbo
+import "@rails/ujs"
+Rails.start()
+console.log("Rails UJS initialized")
+import "controllers"
